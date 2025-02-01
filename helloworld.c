20fceed757262e05b80e69ce4cad6973df8d29be
T.Yashwanth Reddy <yashwanthreddy1887@gmail.com>
2025-02-01 15:32:54 +0530

    Create helloworld.c

---
+++ helloworld.c
@@ -0,0 +1,8 @@
+#include <stdio.h> // Include the standard input/output library
+
+// Main function - entry point of the program
+int main() {
+    // Print "Hello, World!" to the console
+    printf("Hello, World!\n");
+  return 0;
+}
